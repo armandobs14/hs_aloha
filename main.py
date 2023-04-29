87b5228e3537714751b746121159f68df67cc1d6
Armando Barbosa <armandobs14@gmail.com>
2023-04-29 15:02:52 -0300

    Atualiza configuração de experimento

--- main.py
+++ main.py
@@ -3,23 +3,23 @@
 from aloha.aloha import Aloha
 
 if __name__ == "__main__":
-    s_aloha = {
-        "subnets": 2,
-        "nodes_per_subnet": 3,
-        "generate_interval": -1,
-        "head_node_generate": False,
-        "head_node_coin": False,
-        "max_loop": -1,
-    }
+    # s_aloha = {
+    #     "subnets": 1,
+    #     "nodes_per_subnet": 3,
+    #     "generate_interval": -1,
+    #     "head_node_generate": False,
+    #     "head_node_coin": False,
+    #     "max_loop": -1,
+    # }
 
     # Hierarchical Slotted Aloha
     hs_aloha = {
         "subnets": 2,
         "nodes_per_subnet": 3,
-        "generate_interval": 10,
+        "generate_interval": -1,
         "head_node_generate": True,
         "head_node_coin": True,
-        "max_loop": -1,
+        "max_loop": 100,
     }
 
-    (Aloha(**s_aloha).create().start().analyse())
+    (Aloha(**hs_aloha).create().start().analyse())
