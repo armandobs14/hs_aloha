043a690d3f3d5560b4d31f1c37524d4463901144
armandobs14 <armandobs14@gmail.com>
2023-11-15 23:48:15 +0000

    Adding shell.nix

---
+++ shell.nix
@@ -0,0 +1,9 @@
+{ pkgs ? import <nixpkgs> {} }:
+(pkgs.buildFHSUserEnv {
+  name = "pipzone";
+  targetPkgs = pkgs: (with pkgs; [
+    python310
+    python310Packages.pip
+  ]);
+  runScript = "bash";
+}).env
